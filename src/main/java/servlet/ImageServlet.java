55c70cf58ad2001af9bc5b98623ec1d9613a7fa9
zxy960307 <zhangxingyu960307@163.com>
2019-03-30 13:36:57 +0800

    git commit -m 'feat: 开始imageservlet部分。'

---
+++ src/main/java/servlet/ImageServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by 41463 on 2019/3/27.
+ */
+public class ImageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
